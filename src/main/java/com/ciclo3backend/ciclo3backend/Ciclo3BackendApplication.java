886047cb794aa49c436a09b814983685287f1301
Jeison David Hernandez Rayo <jeison..03@hotmail.com>
2021-11-28 15:30:07 -0500

    primer intento

---
+++ src/main/java/com/ciclo3backend/ciclo3backend/Ciclo3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.ciclo3backend.ciclo3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ciclo3BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ciclo3BackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ciclo3backend/ciclo3backend/Model/Admin.java
@@ -0,0 +1,17 @@
+package com.ciclo3backend.ciclo3backend.Model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Jeison Hernandez
+ */
+@Entity
+@Table(name="Admin")
+public class Admin {
+}
